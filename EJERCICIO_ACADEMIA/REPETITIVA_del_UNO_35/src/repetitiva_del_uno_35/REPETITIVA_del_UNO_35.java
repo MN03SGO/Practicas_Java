c76da156812aa8ad14366c20a267479520ca05a6
MN03SGO <146805872+MN03SGO@users.noreply.github.com>
2026-01-21 23:01:09 -0600

    Java

---
+++ EJERCICIO_ACADEMIA/REPETITIVA_del_UNO_35/src/repetitiva_del_uno_35/REPETITIVA_del_UNO_35.java
@@ -0,0 +1,22 @@
+
+package repetitiva_del_uno_35;
+
+public class REPETITIVA_del_UNO_35 {
+
+    public static void main(String[] args) {
+        //FOR 
+//     for( int numero = 1; numero<=35; numero ++){
+//         System.out.println("Los numeros son: "+numero);
+//     }
+//    }
+//    
+    //CON EL WHILE
+    int numero = 1;
+    while(numero <=35){
+        System.out.println("NUM ES: "+numero++);
+    }
+
+
+    }
+
+}
