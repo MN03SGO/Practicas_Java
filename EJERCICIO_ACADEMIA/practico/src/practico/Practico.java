546060ac40c7904ed47aa57f1200fc4e4ad6d1d4
MN03SGO <146805872+MN03SGO@users.noreply.github.com>
2026-02-07 01:24:03 -0600

    Reca

--- EJERCICIO_ACADEMIA/practico/src/practico/Practico.java
+++ EJERCICIO_ACADEMIA/practico/src/practico/Practico.java
@@ -8,12 +8,21 @@ public class Practico {
         System.out.println("**PARQUEO**");
         String placa = "";
 
-        System.out.println("-- INGRESE EL NUMERO DE PLACAS --");
-        Scanner entrada = new Scanner(System.in);
-        placa = entrada.nextLine();
+        int cont1 = 0;
+        int cont2 = 0;
+        int cont3 = 0;
 
+        double diaTOT = 0;
+        Scanner entrada = new Scanner(System.in);
+
+
+        
 
         while(!placa.equalsIgnoreCase("fin")){
+            System.out.println("-- INGRESE EL NUMERO DE PLACAS --");
+            entrada = new Scanner(System.in);
+            placa = entrada.next();
+
             System.out.println("** LOS SERVICIOS DISPONIBLES SON: **");
             System.out.println("1- Por hora");
             System.out.println("2- Media jornada");
@@ -34,35 +43,33 @@ public class Practico {
 
                     int totaHORAS = (servi*3);
                     System.out.println("--Su total a pagar es de: "+totaHORAS);
+                    cont1++;
+                    diaTOT=diaTOT+totaHORAS;
                 }
-                else {
-                    if (servi== 2) {
-                        System.out.println("** Servicio seleccionado: Media jornada **");
-                        System.out.println("El valor por el servicio de 5h es de $15 y posee un descuento del 5%");
-                        int totDESC1 = 15-(15*0.05);
-                        System.out.println("--Su total a pagar es de: " + totDESC1);
-                    }
-                    else if(servi==3){
-                        System.out.println("** Servicio seleccionado: Jornada completa **");
-                        System.out.println("El valor por el servicio de 30h es de $30 y posee un descuento del 5%");
-                        int des2 = (30*0.10);
-                    }
+                else if (servi== 2) {
+                    System.out.println("** Servicio seleccionado: Media jornada **");
+                    System.out.println("El valor por el servicio de 5h es de $15 y posee un descuento del 5%");
+                    double totDESC1 = 15-(15*0.05);
+                    System.out.println("--Su total a pagar es de: " + totDESC1);
+                    cont2++;
+                    diaTOT=diaTOT+totDESC1;
                 }
-
-
+                else{
+                    System.out.println("** Servicio seleccionado: Jornada completa **");
+                    System.out.println("El valor por el servicio de 30h es de $30 y posee un descuento del 5%");
+                    double des2 = 30-(30*0.10);
+                    System.out.println("--Su total a pagar es de: " + des2);
+                    cont3++;
+                    diaTOT=diaTOT+des2;
+                }
             }
-
-
-            
-
-
-
         }
+        System.out.println("");
+        System.out.println("-- Cantidad de servicio por hora: "+cont1);
+        System.out.println("-- Cantidad de servicio por media jornada: "+cont2);
+        System.out.println("-- Cantidad de servicio por jornada completa: "+cont3);
+        System.out.println("-- Total de ganancia del dia : $"+diaTOT);
         
-
-    
-
-
 
     }
     
