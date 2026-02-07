db82c6121c77c537499e8016af8962fe6995281f
MN03SGO <146805872+MN03SGO@users.noreply.github.com>
2026-02-07 01:04:04 -0600

    Avanze

--- EJERCICIO_ACADEMIA/practico/src/practico/Practico.java
+++ EJERCICIO_ACADEMIA/practico/src/practico/Practico.java
@@ -5,76 +5,72 @@ import java.util.Scanner;
 
 public class Practico {
     public static void main(String[] args) {
-        System.out.println("** PARQUEO **");
-        
-        String placa = ""; 
-        Scanner entra = new Scanner(System.in);
-        
-        
-        while (!placa.equalsIgnoreCase("fin")){
-            System.out.println("- Ingrese la placa");
-            placa = entra.nextLine();
-            
-            
-            System.out.println("- Ingrese el tipo de servicio");
+        System.out.println("**PARQUEO**");
+        String placa = "";
+
+        System.out.println("-- INGRESE EL NUMERO DE PLACAS --");
+        Scanner entrada = new Scanner(System.in);
+        placa = entrada.nextLine();
+
+
+        while(!placa.equalsIgnoreCase("fin")){
+            System.out.println("** LOS SERVICIOS DISPONIBLES SON: **");
             System.out.println("1- Por hora");
-            System.out.println("2- Media jornada (5 horas)");
-            System.out.println("3- Jornada completa (10 horas)");
-            
-            
-            int servicio ;
-            entra = new Scanner(System.in);
-            servicio = entra.nextInt();
-            
-            if (servicio<0 ||servicio>3){
-                System.out.println("Tipo de servicio invalido");
-            }else{
-                if (servicio ==1){
-                    System.out.println(" **- SERVICIO SELECCIONADO: POR HORA -**");
-                    System.out.println("- Ingrese cant Horas por el servicio");
-                    entra = new Scanner(System.in);
-                    int cantHoras = entra.nextInt();
-                    int TotaPagar = (cantHoras*3); 
-                    System.out.println("** EL MONTO A PAGAR ES DE: "+cantHoras);
-                }else if( servicio == 2){
-                    System.out.println("**- SERVICIO SELECCIONADO: MEDIA JORNADA -**");
-                    System.out.println("");
-                    
-                }
-                    
-                    
-                    
+            System.out.println("2- Media jornada");
+            System.out.println("3- Jornada completa");
+
+            entrada = new Scanner(System.in);
+            int servi = entrada.nextInt();
+
+            if(servi<=0 || servi>3){
+                System.out.println("Peticion invalida");
             }
-              
+            else{
+                if(servi == 1){
+                    System.out.println("** Servicio seleccionado: Por hora **");
+                    System.out.println("-- INGRESE LAS HORAS --");
+                    entrada = new Scanner(System.in);
+                    servi = entrada.nextInt();
+
+                    int totaHORAS = (servi*3);
+                    System.out.println("--Su total a pagar es de: "+totaHORAS);
+                }
+                else {
+                    if (servi== 2) {
+                        System.out.println("** Servicio seleccionado: Media jornada **");
+                        System.out.println("El valor por el servicio de 5h es de $15 y posee un descuento del 5%");
+                        int totDESC1 = 15-(15*0.05);
+                        System.out.println("--Su total a pagar es de: " + totDESC1);
+                    }
+                    else if(servi==3){
+                        System.out.println("** Servicio seleccionado: Jornada completa **");
+                        System.out.println("El valor por el servicio de 30h es de $30 y posee un descuento del 5%");
+                        int des2 = (30*0.10);
+                    }
+                }
+
+
+            }
+
+
             
-            
-            
-            
-            
-           
-          
-            
-            
-            
-            
-            
-            
-            
-          
-           
-        
+
+
+
         }
         
-  
-        
- 
-           
-        
-        
-        
-        
-        
-       
+
+    
+
+
+
     }
+    
+
+
+        
+        
+    
+    
     
 }
